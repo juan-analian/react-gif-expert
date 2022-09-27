f5c752444a8b590b7c1e4548d18b4642bd0d43fa
Juan Pablo Analian <juananalian@gmail.com>
2022-09-27 16:02:46 -0300

    agregado subtitulo a la pantall ppal

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -16,8 +16,8 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      <h1>Gift Expert App</h1>
-
+      <h1>Gift Expert App </h1>
+      <p>React 18 + vite</p>
       
       <AddCategory  onNewCategory={ onAddCategory } />
     
